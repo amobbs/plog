65097a155036265a8d255eefef951372d1fccc09
Dave <dave@4mation.com.au>
2013-08-21 16:06:33 +1000

    AddeD: auth (broken)

--- src/app/modules/auth/auth.js
+++ src/app/modules/auth/auth.js
@@ -6,9 +6,9 @@ angular.module( 'Preslog.auth', [
         'titleService'
     ])
 
-    /**
-     * Login Route
-     */
+/**
+ * Login Route
+ */
     .config(function(stateHelperProvider) {
         stateHelperProvider.addState('publicLayout.login', {
             url: '/login',
@@ -22,9 +22,52 @@ angular.module( 'Preslog.auth', [
     })
 
 
-    /**
-     * Controller
-     */
+/**
+ * Executed at startup
+ * - Checks users authentication
+ */
+    .run(function ($rootScope, $location, Restangular, userService) {
+        if (typeof $rootScope.global === 'undefined') {
+            $rootScope.global = {};
+        }
+
+        var requestedPath = '/';
+        $rootScope.global.loggedIn = false;
+        userService.getUser().then(function (user) {
+            $rootScope.global.user = user;
+            $rootScope.global.loggedIn = true;
+        });
+
+        $rootScope.$on('event:auth-loginRequired', function () {
+            var path = $location.path();
+            if ('/login' != path) {
+                requestedPath = path;
+            }
+            $rootScope.global.loggedIn = false;
+            $rootScope.global.user = {};
+            $location.path('/login');
+        });
+
+        $rootScope.$on('event:auth-loginConfirmed', function (event, data) {
+            $rootScope.global.user = data;
+            $rootScope.global.loggedIn = true;
+            $location.path(requestedPath);
+        });
+
+        $rootScope.global.logout = function () {
+            $rootScope.global.user = {};
+            $rootScope.global.loggedIn = false;
+            Restangular.all('account').customGET('logoff').then(function() {
+                $rootScope.$broadcast('event:auth-loggedOut');
+                $location.path('/login');
+            });
+        };
+    })
+
+
+/**
+ * Controller
+ */
     .controller( 'AuthCtrl', function AuthController( $scope, titleService ) {
         console.log('auth');
         titleService.setTitle( 'Login' );
