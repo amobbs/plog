fb9c263ecdc938d1b20a8d4a45169a17d8e3ff57
Dave <dave@4mation.com.au>
2013-09-19 14:19:38 +1000

    Added: comments to TitleService Bugfix: TitleService no longer includes the suffix element if it's empty. Bugfix: TitleService now handles string correctly and casts them to arrays.

--- src/common/services/titleService/titleService.js
+++ src/common/services/titleService/titleService.js
@@ -6,54 +6,88 @@
 angular.module('titleService', [])
     .factory('titleService', function ( $document ) {
 
+        // Title suffix
         var suffix = '';
-        var delimiter = '';
+
+        // Title delimiter
+        var delimiter = ' - ';
 
         var service = {
 
+
+            /**
+             * Set the Delimiter to use in the title
+             * @param string
+             */
             setDelimiter: function( string )
             {
                 delimiter = string;
             },
 
+
+            /**
+             * Get the delimiter that has been set
+             * @returns {string}
+             */
             getDelimiter: function()
             {
                 return delimiter;
             },
 
+
+            /**
+             * Set the suffix for the title
+             * @param string
+             */
             setSuffix: function( string )
             {
                 suffix = string;
             },
 
+
+            /**
+             * Get the suffix for the title
+             * @returns {string}
+             */
             getSuffix: function()
             {
                 return suffix;
             },
 
+
+            /**
+             * Set the current page title
+             * @param title
+             */
             setTitle: function( title )
             {
                 // Put to an array if not one already
-                if (!title instanceof Array)
+                if (!(title instanceof Array))
                 {
                     title = [ title ];
                 }
 
-                // Add suffix
-                title.push(suffix);
+                // Add suffix, if suffix is set
+                if (suffix.length) {
+                    title.push(suffix);
+                }
 
-                console.log(title);
-
+                // Add delimiters and cast to string
                 title = title.join( delimiter );
 
+                // Update title
                 $document.prop('title', title);
             },
 
+
+            /**
+             * Get the current page title
+             * @returns {*}
+             */
             getTitle: function()
             {
                 return $document.prop('title');
             }
-
 
         };
 
