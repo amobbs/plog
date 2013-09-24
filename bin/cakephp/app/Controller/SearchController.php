ff026b6dc3ce06176c299682bf40a63a12d87ef2
Dave <dave@4mation.com.au>
2013-09-25 09:21:20 +1000

    Added: Pleaseholder search code

--- bin/cakephp/app/Controller/SearchController.php
+++ bin/cakephp/app/Controller/SearchController.php
@@ -1,15 +1,14 @@
 <?php
 
-/**
- * Class SearchController
- */
-
 App::uses('AppController', 'Controller');
 App::import('vendor', 'JqlParser/JqlParser');
 
 use Swagger\Annotations as SWG;
 use JqlParser\JqlParser;
 
+/**
+ * Class SearchController
+ */
 class SearchController extends AppController
 {
     public $uses = array('Search', 'JqlParser');
@@ -20,14 +19,27 @@ class SearchController extends AppController
      * @SWG\Operation(
      *      partial="search",
      *      summary="Return log list based on POST JQL search criteria",
-     *      notes="Users can only search across those Clients to which they have access."
+     *      notes="Users can only search across those Clients to which they have access.",
+     *      @SWG\Parameters(
+     *          @SWG\Parameter(
+     *              name="query",
+     *              paramType="query",
+     *              dataType="string",
+     *              required="true",
+     *              description="jql to search on"
+     *          )
+     *      )
      * )
      */
     public function search()
     {
-        // TODO
-        $this->set('todo', 'Search');
-        $this->set('_serialize', array('todo'));
+        // Perform search
+        // Returns Logs and Options to accompany
+        $return = $this->executeSearch( $this->request->query );
+
+        // Return search result
+        $this->set($return);
+        $this->set('_serialize', array_keys($return));
     }
 
 
@@ -37,15 +49,92 @@ class SearchController extends AppController
      * @SWG\Operation(
      *      partial="search.export",
      *      summary="Instigate download of XLS containing search results",
-     *      notes="Replicates the functionality of Search, with XLS output."
+     *      notes="Replicates the functionality of Search, with XLS output.",
+     *      @SWG\Parameters(
+     *          @SWG\Parameter(
+     *              name="query",
+     *              paramType="query",
+     *              dataType="string",
+     *              required="true",
+     *              description="jql to search on"
+     *          )
+     *      )
      * )
      */
     public function export()
     {
-        // TODO
-        $this->set('todo', 'Search Export');
-        $this->set('_serialize', array('todo'));
+        // Perform search
+        // Returns Logs and Options to accompany
+        $return = $this->executeSearch( $this->request->query );
+
+        // Generate export XLS from data
+        $this->set($return);
+        $this->viewClass = 'View';
+        $this->render('export_xls');
+
+        // Complete request
+        exit();
     }
+
+
+    /**
+     * Perform the search operation and return a series of log data sufficient for search results.
+     * TODO: This should really be paginated to avoid problems with large result sets
+     */
+    protected function executeSearch( $params )
+    {
+        // TODO
+
+        $results = array();
+        $options = array();
+
+        // Validate: Search criteria must not be empty
+        if ( !isset($params['query']) || empty($params['query']) )
+        {
+            $this->errorBadRequest(array('message'=>"Search parameters must not be empty. Please supply a valid JQL query to the 'query' variable."));
+        }
+
+        // Get the query
+        $query = $params['query'];
+
+        // Validate: If the users permissions are "single-client",
+        // add a query value which ensures they only get results from their own client_id
+        if (false)
+        {
+            $query .= 'AND client_id = my_client_id';
+        }
+
+        // Translate query to Mongo
+        $query = $query;
+
+        // Do query
+        $results = array($query);
+
+        // Skim results for client types
+        foreach ($results as $k=>$result)
+        {
+            // Collate the list of clients for fetching the field format
+            $clients[] = $result['client_id'];
+
+            // Drop the Accountability and Status fields if we don't have permission
+            if (false)
+            {
+                unset($results[$k]['accountability']);
+                unset($results[$k]['status']);
+            }
+        }
+
+        // Fetch the client field opts from the Log system
+        // Return an array of options by client
+        foreach ($clients as $client)
+        {
+            $options[$client] = $this->Client->getOptionsByClientId( $client );
+        }
+
+        // Return the Results and the corresponding Client opts
+        return array( $results, $options );
+    }
+
 
     /**
      * Fetch params for the Search Query Builder Wizard
---
+++ bin/cakephp/app/View/Search/export_xls.ctp
@@ -0,0 +1,10 @@
+<?php
+
+// TODO
+
+// export all log data to XLS
+
+foreach ($logs as $log)
+{
+    $options['blah'];
+}
