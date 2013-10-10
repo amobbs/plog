1dd13345ce355d065048aee7785317a2b8304452
Cody Lundquist <cody@4mation.com.au>
2013-10-10 15:18:18 +1100

    Fixing formatting.

--- bin/cakephp/Vendor/ichikaway/Mongodb/Model/Behavior/SchemaBehavior.php
+++ bin/cakephp/Vendor/ichikaway/Mongodb/Model/Behavior/SchemaBehavior.php
@@ -21,7 +21,6 @@
  * @since           v 1.0 (1-Oct-2013)
  * @license         http://www.opensource.org/licenses/mit-license.php The MIT License
  */
-
 /**
  * SchemaBehavior class
  *
@@ -29,7 +28,8 @@
  * @package         mongodb
  * @subpackage      mongodb.models.behaviors
  */
-class SchemaBehavior extends ModelBehavior {
+class SchemaBehavior extends ModelBehavior
+{
 
     public $name = 'Schema';
 
@@ -37,194 +37,150 @@ class SchemaBehavior extends ModelBehavior {
      * Setup this behaviours
      * - Find the primary key in the Schema and set it on the Model
      */
-    public function setup( Model $Model, $settings = array() )
+    public function setup(Model $Model, $settings = array())
     {
         // Default primary key is _id
         $key = '_id';
-
         // Try to find the Primary Key in the schema
-        foreach ($Model->mongoSchema as $fieldName=>$fieldOptions)
-        {
+        foreach ($Model->mongoSchema as $fieldName => $fieldOptions) {
             // Does it have primary key?
-            if ( isset($fieldOptions['primary']) )
-            {
+            if (isset($fieldOptions['primary'])) {
                 $key = $fieldName;
                 break;
             }
         }
-
         // Primary key set!
         $Model->primaryKey = $key;
     }
 
-
     /**
-     * @param   array     $options
+     * @param   array $options
+     *
      * @return  boolean
      */
-    public function beforeSave( Model $model, $options = array() )
+    public function beforeSave(Model $model, $options = array())
     {
         // Convert data types for save
-        $this->extractModel( $model->data[ $model->name ], $model->mongoSchema);
+        $this->extractModel($model->data[$model->name], $model->mongoSchema);
 
         return true;
     }
 
-
     /**
      * After Find
      * Convert the
-     * @param   array           $results
-     * @param   bool            $primary
+     *
+     * @param   array $results
+     * @param   bool $primary
+     *
      * @return  array|mixed
      */
-    public function afterFind( Model $model, $results, $primary = false )
+    public function afterFind(Model $model, $results, $primary = false)
     {
-        foreach ($results as &$result)
-        {
+        foreach ($results as &$result) {
             // Process the returned doc against the schema doc
-            $this->hydrateModel( $result[ $model->name ], $model->mongoSchema );
+            $this->hydrateModel($result[$model->name], $model->mongoSchema);
         }
 
         return $results;
     }
 
-
     /**
      * From Database to Model
      */
-    public function hydrateModel( &$doc, array $schema )
+    public function hydrateModel(&$doc, array $schema)
     {
         // Skip non-arrays
-        if (!is_array($doc))
-        {
+        if (! is_array($doc)) {
             return;
         }
-
         // Check for numeric array keys
         $numericKeys = array_filter(array_keys($doc), 'is_int');
-
         // If an array, recuse each one on the schema type.
-        if (sizeof($numericKeys) == sizeof($doc))
-        {
-            foreach ($doc as &$docItem)
-            {
+        if (sizeof($numericKeys) == sizeof($doc)) {
+            foreach ($doc as &$docItem) {
                 $this->hydrateModel($docItem, $schema);
             }
 
             return;
         }
-
         // Convert schema
-        foreach ( $schema as $fieldKey=>$fieldOptions )
-        {
+        foreach ($schema as $fieldKey => $fieldOptions) {
             // Null types are ignores. These are dynamic schemas
-            if ($fieldOptions === null)
-            {
+            if ($fieldOptions === null) {
                 continue;
             }
-
             // Skip fields that don't exist
-            if (!isset( $doc[$fieldKey] ))
-            {
+            if (! isset($doc[$fieldKey])) {
                 continue;
             }
-
             // No type and not null implies a sub-document
             // Recurse into the sub-document
-            if (!isset($fieldOptions['type']))
-            {
-                $this->hydrateModel( $doc[$fieldKey], $fieldOptions );
+            if (! isset($fieldOptions['type'])) {
+                $this->hydrateModel($doc[$fieldKey], $fieldOptions);
             }
-
             // Skip non-type fields
-            if ( !isset($fieldOptions['mongoType']) )
-            {
+            if (! isset($fieldOptions['mongoType'])) {
                 continue;
             }
-
             // MongoId
-            if ($fieldOptions['mongoType'] == 'MongoId')
-            {
+            if ($fieldOptions['mongoType'] == 'MongoId') {
                 $doc[$fieldKey] = (string) $doc[$fieldKey];
             }
-
             // MongoDate
-            if ($fieldOptions['mongoType'] == 'MongoDate')
-            {
+            if ($fieldOptions['mongoType'] == 'MongoDate') {
                 $doc[$fieldKey] = date('Y-m-d h:i:s', $doc[$fieldKey]->sec);
             }
         }
     }
 
-
     /**
      * From Model to Database
      */
-    public function extractModel( &$doc, $schema )
+    public function extractModel(&$doc, $schema)
     {
         // Non-arrays are not processed
-        if ( !is_array($doc) )
-        {
+        if (! is_array($doc)) {
             return;
         }
-
         // Check for numeric array keys
         $numericKeys = array_filter(array_keys($doc), 'is_int');
-
         // If an array, recuse each one on the schema type.
-        if (sizeof($numericKeys) == sizeof($doc))
-        {
-            foreach ($doc as &$docItem)
-            {
+        if (sizeof($numericKeys) == sizeof($doc)) {
+            foreach ($doc as &$docItem) {
                 $this->extractModel($docItem, $schema);
             }
 
             return;
         }
-
         // Convert schema
-        foreach ( $schema as $fieldKey=>$fieldOptions )
-        {
+        foreach ($schema as $fieldKey => $fieldOptions) {
             // Null types are ignores. These are dynamic schemas
-            if ($fieldOptions === null)
-            {
+            if ($fieldOptions === null) {
                 continue;
             }
-
             // Skip fields that don't exist
-            if (!isset( $doc[$fieldKey] ))
-            {
+            if (! isset($doc[$fieldKey])) {
                 continue;
             }
-
             // No type and not null implies a sub-document
             // Recurse into the sub-document
-            if (!isset($fieldOptions['type']))
-            {
-                $this->extractModel( $doc[$fieldKey], $fieldOptions );
+            if (! isset($fieldOptions['type'])) {
+                $this->extractModel($doc[$fieldKey], $fieldOptions);
             }
-
             // Skip non-type fields
-            if ( !isset($fieldOptions['mongoType']) )
-            {
+            if (! isset($fieldOptions['mongoType'])) {
                 continue;
             }
-
             // MongoId
-            if ($fieldOptions['mongoType'] == 'MongoId')
-            {
-                $doc[$fieldKey] = new MongoId( $doc[$fieldKey] );
+            if ($fieldOptions['mongoType'] == 'MongoId') {
+                $doc[$fieldKey] = new MongoId($doc[$fieldKey]);
             }
-
             // MongoDate
-            if ($fieldOptions['mongoType'] == 'MongoDate')
-            {
-                $doc[$fieldKey] = new MongoDate( strtotime($doc[$fieldKey]) );
+            if ($fieldOptions['mongoType'] == 'MongoDate') {
+                $doc[$fieldKey] = new MongoDate(strtotime($doc[$fieldKey]));
             }
         }
     }
-
-
 }
 
