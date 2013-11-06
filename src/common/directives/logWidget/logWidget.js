548295e9e4122a78e3e38bb4f18d755d1499e4ff
Kieran Yeates <kieran.yeates@4mation.com.au>
2013-11-06 14:49:36 +1100

    bugfix: click on log list should redirect to edit log

--- src/common/directives/logWidget/logWidget.js
+++ src/common/directives/logWidget/logWidget.js
@@ -38,6 +38,7 @@ angular.module('logWidget', [])
 
                 scope.redirectToLog = function(logId)
                 {
+                    logId = logId.replace('#', '');
                     window.location = '/logs/' + logId;
                 };
 
