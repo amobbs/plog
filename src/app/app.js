423860f71b660405b15a824b16e50f568bd7b89e
Dave Newson <vince@shiftrunstop.com>
2013-11-11 00:12:55 +1100

    Attempted to add permissions checking with userService (named userSvc in views). Ran into this issue: https://github.com/angular/angular.js/commit/5dc35b527b3c99f6544b8cb52e93c6510d3ac577
    Todo: Find a way to resolve the promise, or the data, prior to loading templates and put that into the view. Perhaps we can do something where the function for checkPermissions references the readUser() data instead of going off and using async promise code.

--- src/app/app.js
+++ src/app/app.js
@@ -9,15 +9,23 @@ angular.module('Preslog', [
     ])
 
     .config(function myAppConfig($stateProvider, $urlRouterProvider, $locationProvider, RestangularProvider) {
+
+        // APi directory
         RestangularProvider.setBaseUrl('/api');
     })
 
-    .run(function run(titleService) {
+    .run(function run(titleService, $rootScope, userService, $log) {
+
+        // Title configuration
         titleService.setSuffix('Preslog');
         titleService.setDelimiter(' - ');
+
+        // accessible global services
+        $rootScope.$log = $log;
+        $rootScope.userSvc = userService;      // Bugfix: Naming the rootScope var the same as the service seems to break things
     })
 
-    .controller('AppCtrl', function AppCtrl($scope, $location) {
+    .controller('AppCtrl', function AppCtrl($scope) {
         $scope.$on('loadingHandler.loading', function(event, loading) {
             $scope.waitingOnHTTP = loading;
         });
--- src/common/directives/permissions/permission.js
+++ src/common/directives/permissions/permission.js
@@ -1,6 +1,18 @@
+/**
+ * Angular permissions related directives
+ * @author  Dave Newson <dave@4mation.com.au>
+ * @copy    4mation Technologies
+ * @link    http://www.4mation.com.au
+ */
 angular.module('permission', [
         'userService'
     ])
+
+    /**
+     * Permission directive
+     * - Only compile this element into existance if the user has the correct permissions.
+     * - Element will not show up if the users permissions change; it is permanently excluded.
+     */
     .directive('permission', ['userService', function (userService) {
 
         return {
@@ -23,4 +35,28 @@ angular.module('permission', [
                 });
             }
         };
-    }]);
+    }])
+
+    /**
+     * ng-Permissions directive
+     * - Only compile this element into statement passed evalutes to true.
+     * - Element will not show up if the users permissions change; it is permanently excluded.
+     */
+    .directive('ngPermission', [function () {
+
+        return {
+            priority: 1000,
+            compile: function(element, attrs) {
+
+                // If not allowed
+                if (!attrs.ngPermission)
+                {
+                    // Remove the elements
+                    var children = element.children();
+                    children.remove();
+                    element.remove();
+                }
+            }
+        };
+    }])
+;
