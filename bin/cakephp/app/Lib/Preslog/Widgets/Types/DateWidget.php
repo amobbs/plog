07f9133d483479a6c92471c26f34b4705b0ccf58
Kieran Yeates <kieran.yeates@4mation.com.au>
2013-11-12 13:42:57 +1100

    bugfix: date picker passing back incorrect date

--- bin/cakephp/app/Lib/Preslog/Widgets/Types/DateWidget.php
+++ bin/cakephp/app/Lib/Preslog/Widgets/Types/DateWidget.php
@@ -17,24 +17,35 @@ class DateWidget extends Widget {
             $this->details = array();
         }
 
-        if (isset($data['details']))
+        if (!isset($data['details']))
         {
-            $this->details['period'] = isset($data['details']['period']) ? $data['details']['period'] : 'Week';
 
-            if (isset($data['details']['defaultStart']))
-            {
-                $this->details['defaultStart'] = $data['details']['defaultStart'];
-                $clause = new Clause("created = " . $data['details']['defaultStart'], true);
-                $this->details['start'] = date('c', $clause->getFunctionEvaluated());
-            }
+            $data = array(
+                'details' => array(
+                    'period' => 'Day',
+                    'defaultStart' => 'startofday()',
+                    'defaultEnd' => 'endofday()',
+                ),
+            );
 
-            if (isset($data['details']['defaultEnd']))
-            {
-                $this->details['defaultEnd'] = $data['details']['defaultEnd'];
-                $clause = new Clause("created = " . $data['details']['defaultEnd'], true);
-                $this->details['end'] = date('c',$clause->getFunctionEvaluated());
-            }
         }
+
+        $this->details['period'] = isset($data['details']['period']) ? $data['details']['period'] : 'Day';
+
+        if (isset($data['details']['defaultStart']))
+        {
+            $this->details['defaultStart'] = $data['details']['defaultStart'];
+            $clause = new Clause("created = " . $data['details']['defaultStart'], true);
+            $this->details['start'] = date('c', $clause->getFunctionEvaluated());
+        }
+
+        if (isset($data['details']['defaultEnd']))
+        {
+            $this->details['defaultEnd'] = $data['details']['defaultEnd'];
+            $clause = new Clause("created = " . $data['details']['defaultEnd'], true);
+            $this->details['end'] = date('c',$clause->getFunctionEvaluated());
+        }
+
 
         parent::__construct($data);
     }
