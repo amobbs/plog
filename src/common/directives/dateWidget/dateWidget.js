63d533f7eb19a449cfbd97c5cc9564b2a098d571
kieran.yeates <kieran.yeates@4mation.com.au>
2013-12-12 14:11:58 +1100

    RAPID-5918-date selector bug

--- src/common/directives/dateWidget/dateWidget.js
+++ src/common/directives/dateWidget/dateWidget.js
@@ -14,27 +14,48 @@ angular.module('dateWidget', [])
                 session: '='
             },
             link: function( scope, element, attrs, ctrl ) {
+                scope.startOfDay = function(start)
+                {
+                    var s = new Date(start);
+                    s.setHours(0);
+                    s.setMinutes(0);
+                    s.setSeconds(0);
+
+                    return s;
+                };
+
+                scope.endOfDay = function(end)
+                {
+                    var e = new Date(end);
+                    e.setHours(23);
+                    e.setMinutes(59);
+                    e.setSeconds(59);
+
+                    return e;
+                };
+
                 scope.forward = function()
                 {
-                    var start = new Date(scope.session.start);
-                    var end = new Date(scope.session.end);
+                    var start = scope.startOfDay(new Date(scope.session.start));
+                    var end = scope.endOfDay(new Date(scope.session.end));
 
                     switch (scope.session.period)
                     {
                         case 'Week':
-                            start.setDate(start.getDate() + 7);
-                            end = new Date(start);
-                            end.setDate(start.getDate() + 7);
+                            start.setDate(start.getDate() + 7); //take the current start and add 7 days
+                            end = scope.endOfDay(new Date(start)); //get a copy of start but with time set to end
+                            end.setDate(start.getDate() + 6); //set date to now plus 6 so it is at the very end of the week
                             break;
                         case 'Day':
-                            start.setDate(start.getDate() + 1);
-                            end = new Date(start);
-                            end.setDate(start.getDate() + 1);
+                            start.setDate(start.getDate() + 1); //get now and add a day
+                            end = scope.endOfDay(new Date(start)); //get a copy of start but time set to end of day
+                            end.setDate(start.getDate()); //send date to same as start but time is at end
                             break;
                         case 'Month':
-                            start.setMonth(start.getMonth() + 1);
-                            end = new Date(start);
-                            end.setMonth(start.getMonth() + 1);
+                            start.setMonth(start.getMonth() + 1); //add 1 to start month
+                            end = scope.endOfDay(new Date(start)); //get copy of start but time set to end of day
+                            end.setMonth(start.getMonth() + 1); //take the new start month and add one
+                            end.setDate(start.getDate() - 1); //take off a day so it is on last day of that month's period
                             break;
                     }
 
@@ -42,50 +63,57 @@ angular.module('dateWidget', [])
                     scope.session.end = end;
                 };
 
+                /**
+                 * take the current start date and change the end date to be 1 unit of the new period selected (eg: +1 week for week)
+                 */
                 scope.changePeriod = function()
                 {
-                    var start = new Date(scope.session.start);
+
+                    var start = scope.startOfDay(new Date(scope.session.start));
                     var end = new Date(start);
 
 
                     switch (scope.session.period)
                     {
                         case 'Week':
-                            end.setDate(start.getDate() + 7);
+                            end.setDate(start.getDate() + 6);
                             break;
                         case 'Day':
                             end.setDate(start.getDate() + 1);
                             break;
                         case 'Month':
                             end.setMonth(start.getMonth() + 1);
+                            end.setDate(start.getDate() - 1);
                             break;
                     }
 
                     scope.session.start = start;
-                    scope.session.end = end;
+                    scope.session.end = scope.endOfDay(end);
                 };
 
                 scope.backward = function()
                 {
-                    var start = new Date(scope.session.start);
-                    var end = new Date(scope.session.end);
+
+                    var start = scope.startOfDay(new Date(scope.session.start));
+                    var end = scope.endOfDay(new Date(scope.session.end));
 
                     switch (scope.session.period)
                     {
                         case 'Week':
-                            start.setDate(start.getDate() - 7);
-                            end = new Date(start);
-                            end.setDate(start.getDate() + 7);
+                            start.setDate(start.getDate() - 7); //take the current start and add 7 days
+                            end = scope.endOfDay(new Date(start)); //get a copy of start but with time set to end
+                            end.setDate(start.getDate() + 6); //set date to now plus 6 so it is at the very end of the week
                             break;
                         case 'Day':
-                            start.setDate(start.getDate() - 1);
-                            end = new Date(start);
-                            end.setDate(start.getDate() + 1);
+                            start.setDate(start.getDate() - 1); //get now and add a day
+                            end = scope.endOfDay(new Date(start)); //get a copy of start but time set to end of day
+                            end.setDate(start.getDate()); //send date to same as start but time is at end
                             break;
                         case 'Month':
-                            start.setMonth(start.getMonth() - 1);
-                            end = new Date(start);
-                            end.setMonth(start.getMonth() + 1);
+                            start.setMonth(start.getMonth() - 1); //add 1 to start month
+                            end = scope.endOfDay(new Date(start)); //get copy of start but time set to end of day
+                            end.setMonth(start.getMonth() + 1); //take the new start month and add one
+                            end.setDate(start.getDate() - 1); //take off a day so it is on last day of that month's period
                             break;
                     }
 
