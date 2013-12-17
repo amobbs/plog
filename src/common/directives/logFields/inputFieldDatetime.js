115d874f164f2d27a812fca28b57550add23787d
kieran.yeates <kieran.yeates@4mation.com.au>
2013-12-18 09:57:29 +1100

    RAPID-5952-when selecting 'other' for segregation it shows a number instead of entered text

--- src/common/directives/logFields/inputFieldDatetime.js
+++ src/common/directives/logFields/inputFieldDatetime.js
@@ -170,9 +170,14 @@ angular.module('inputFieldDatetime', [])
                 var date = new Date( ctrl.$modelValue );
                 var timeParts = value.split(':');
 
-                if (timeParts.length < 3)
+                if (timeParts.length == 1)
                 {
-                    timeParts = ["12", "00", "00"];
+                    timeParts.push(00);
+                    timeParts.push(00);
+                }
+                else if (timeParts.length == 2)
+                {
+                    timeParts.push(00);
                 }
 
                 var newDate = new Date('0001-01-01 00:00:00');
