e473cbe2f407389e2dc5e4154e05a691eba2d08b
kieran.yeates <kieran.yeates@4mation.com.au>
2014-05-19 16:24:35 +1000

    testing strtotime conversion on production. sorry.

--- bin/cakephp/app/Model/Log.php
+++ bin/cakephp/app/Model/Log.php
@@ -450,6 +450,9 @@ class Log extends AppModel
             );
         }
 
+        $log->info("findByQuery criteria");
+        $log->info($criteria);
+
         //actually do the query and return result
         $mongo = $this->getMongoDb();
         $data = $mongo->selectCollection('logs')->aggregate($criteria);
